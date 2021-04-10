a2361a6c5fe0feec8b4175f55de6a6665185c393
bryanm236 <bmuir@rams.colostate.edu>
2021-04-10 17:53:40 +0000

    My Second Project Week 13

---
+++ MySecondProject/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+
+function MyApp() {
+  
+  function getFullName(fName, mName, lName) {
+    return fName + " " + mName + " " + lName;
+  }
+  
+  const pet = "Dog";
+  return (
+
+    <View>
+    <Text>
+      {"\n\n\n\n\n\n"}
+      
+      Hello, I'm a student in CIS340! {"\n"}
+      My full name is {getFullName("Bryan", "Michael", "Muir")} {"\n"}
+      I will have a {pet}!
+    </Text>
+    <TextInput 
+    style = {{
+      height: 40,
+      borderColor: "blue",
+      borderWidth: 1
+
+    }}
+    defaulValue = "Enter Your Input"/> 
+    </View>
+     
+    
+  );
+}
+
+export default function MultiComp(){
+  return(
+
+    <View style = {{
+      flex: 1,
+      justifyContent: 'Center',
+      alignItems: 'center'
+    
+    }}>
+      <Text>  {"\n\n\n\n\n"} Welcome to My Class</Text>
+      
+      <MyApp />
+      <MyApp />
+      <MyApp />
+      <MyApp />
+      <MyApp />
+
+      
+
+    </View>
+
+  )
+
+}
+
